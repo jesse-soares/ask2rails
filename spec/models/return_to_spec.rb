b3b9f493df5e90191d3696b71c6523f6f3c69bad
Jessé Cirilo Soares <jessecsoares@gmail.com>
2017-07-03 00:12:31 -0300

    Improvements into model ReturnTo test

--- spec/models/return_to_spec.rb
+++ spec/models/return_to_spec.rb
@@ -3,18 +3,27 @@ require "./app/models/return_to"
 
 RSpec.describe ReturnTo do
   it "returns default path when no chosen path has been given" do
-    expect(ReturnTo.new("/default", nil).path).to eql("/default")
+    path = ReturnTo.new("/default", nil).path
+    expect(path).to eql("/default")
   end
 
   it "returns chosen path" do
-    expect(ReturnTo.new("/default", "/chosen").path).to eql("/chosen")
+    path = ReturnTo.new("/default", "/chosen").path
+    expect(path).to eql("/chosen")
   end
 
   it "returns default path for invalid chosen path" do
-    expect(ReturnTo.new("/default", "ínválíd").path).to eql("/default")
+    path = ReturnTo.new("/default", "ínválíd").path
+    expect(path).to eql("/default")
   end
 
   it "returns with query string" do
-    expect(ReturnTo.new("/default", "/chosen?a=1&b=2").path).to eql("/chosen?a=1&b=2")
+    path = ReturnTo.new("/default", "/chosen?a=1&b=2").path
+    expect(path).to eql("/chosen?a=1&b=2")
+  end
+
+  it "ignores host" do
+    path = ReturnTo.new("/default", "http://example.org/chosen").path
+    expect(path).to eql("/chosen")
   end
 end
